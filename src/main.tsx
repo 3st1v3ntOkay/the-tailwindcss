1917079df435fe02d80f784e2213fefc89a760f4
Esteban Jimenez Valdez <88903033+3st1v3ntOkay@users.noreply.github.com>
2025-01-23 15:50:22 -0800

    feat: testing ts alias

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 
 import { Root } from "./routes/root";
 
+import s from "@/"
+
 import "./index.css";
 
 const root = createRoot(document.getElementById("root")!);
