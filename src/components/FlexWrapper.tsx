51d3112c1a0e5cf60dd07bdcd24c5ec68aa0cb1a
Esteban Jimenez Valdez <88903033+3st1v3ntOkay@users.noreply.github.com>
2025-02-18 14:24:27 -0800

    feat: +3 new props to work more comfortable

--- src/components/FlexWrapper.tsx
+++ src/components/FlexWrapper.tsx
@@ -4,21 +4,30 @@ import type { ReactNode } from "react";
 
 type ColorsAvailable = "red" | "orange" | "yellow" | "green" | "blue" | "violet";
 
+type FlexDirection = "row" | "column" | "row-reverse" | "column-reverse";
+
+type FlexGap = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
+
 export function FlexWrapper({
   children,
   mode = "dev",
   color = "blue",
+  gap = "2",
+  direction = "column"
 }: {
   children: ReactNode;
   mode: "dev" | "prod";
   color: ColorsAvailable;
+  gap: FlexGap;
+  direction: FlexDirection;
 }) {
   const whichMode: string = mode === "dev" ? `border-2 border-dashed border-${color}-500 rounded-lg` : "";
 
   return (
     <Flex
-      direction="column"
       className={`${whichMode} p-2`}
+      gap={gap}
+      direction={direction}
     >
       {children}
     </Flex>
